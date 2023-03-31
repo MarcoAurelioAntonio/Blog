67963b717f81e5a1e8bbf97b9542cf9b8c059c0e
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-03-30 21:23:16 -0300

    Seting asociations, validations and methods in Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  # Associations
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  after_save :update_posts_counter
+
+  # Validations
+  validates :title, presence: true, length: { maximum: 250 }, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
+  def recents_comments
+    comments.order('created_at Desc').limit(5)
+  end
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
