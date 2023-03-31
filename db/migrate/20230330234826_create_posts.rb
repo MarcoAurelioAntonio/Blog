545c6becd3ada4cb22a710e55d59afc22adc9905
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-03-30 21:18:38 -0300

    Refactor migrations

--- db/migrate/20230330234826_create_posts.rb
+++ db/migrate/20230330234826_create_posts.rb
@@ -9,5 +9,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+
+    add_foreign_key :posts, :users, column: :author_id
+    add_index :posts, :author_id
   end
 end
--- db/migrate/20230330234940_create_likes.rb
+++ db/migrate/20230330234940_create_likes.rb
@@ -6,5 +6,9 @@ class CreateLikes < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+
+    add_foreign_key :likes, :users, column: :author_id
+    add_index :likes, :author_id
+    add_index :likes, :post_id
   end
 end
