1b5269ec1aaa52cd657f53ca8195ec41f3092df9
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-05 12:15:50 -0300

    User posts show erb, partial and style done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :asc)
   end
 
   def show
-    params[:id]
+    # params[:id]
+    @user = User.find(params[:id])
+    @posts = @user.recents_post
   end
 end
