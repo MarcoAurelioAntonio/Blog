fb5bc63bab27077be1c29a7e383194d7d39b2779
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-05 18:13:20 -0300

    Posts/show done with styles included

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index;	end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.recents_post
+  end
 
   def show
     params[:user_id]
