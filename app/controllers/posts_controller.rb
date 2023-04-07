4a0610126a53b2df02e013b92bb60c1d1d284983
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-07 16:13:24 -0300

    Form new Post done with style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,14 @@ class PostsController < ApplicationController
 
   def show
     # params[:user_id]
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+
+    #@user = User.find(params[:user_id])
+    #if params[:id] == "new"
+    #  @post = Post.new
+    #else
+    #  @post = Post.find(params[:id])
+    #end
   end
 
   # Action for the form
--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,20 @@ Rails.application.routes.draw do
 
   # Route for users
   root 'users#index'
-  get 'users', to: 'users#index', as: 'users'
-  get 'users/:id', to: 'users#show', as: 'user'
+  #get 'users', to: 'users#index', as: 'users'
+  #get 'users/:id', to: 'users#show', as: 'user'
 
   # Route for posts
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  #get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  #get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   
   # Routes for create a new post
-  get 'users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
-  post 'users/:user_id/posts', to: 'posts#create'
+  #get 'users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
+  #post 'users/:user_id/posts', to: 'posts#create'
+
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show, :new, :create]
+  end
 
 end
 
