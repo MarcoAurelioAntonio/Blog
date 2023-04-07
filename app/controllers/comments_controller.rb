678b446f7ae29f9637eb0e785aeaefe68a1b6024
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-07 16:53:02 -0300

    The controler Commnets was created with 'rails g controller Comments new create'
    After that the content of action put it by hand

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to user_post_path(id: current_user.id), notice: 'Comment was successfully created.'
+    else
+      render :new
+    end
   end
 end
