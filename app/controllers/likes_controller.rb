90e6f0ff09a267df7c26ea2462f959fd1405f7ff
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-07 21:36:31 -0300

    Like button working fine

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,19 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.new(like_params)
+    @like.author_id = current_user.id
+    if @like.save
+      
+      redirect_to user_posts_path(@like.post, @like.author)
+    else
+      
+      render 'new'
+    end
+  end
+
+  private
+
+  def like_params
+    params.permit(:author_id, :post_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show, :new, :create] do
+      resources :likes, only: [:create]
       resources :comments, only: [:new, :create]
     end
   end
