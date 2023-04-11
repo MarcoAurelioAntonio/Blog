d6183e2cee4efee8d71ed72e2ccdc9fff0d820b6
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-11 18:45:15 -0300

    Fix reviews requirements

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,35 +2,62 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    subject { get '/users' }
-    before { subject }
+    subject do
+      first_user = User.create(name: 'Tom', photo: '', bio: 'Teacher from Mexico.', posts_counter: 0)
+
+      Post.create(author: first_user, title: 'Title 1', text: 'post text 2', comments_counter: 0, likes_counter: 0)
+      Post.create(author: first_user, title: 'Title 2', text: 'post text 3', comments_counter: 0, likes_counter: 0)
+      Post.create(author: first_user, title: 'Title 3', text: 'post text 4', comments_counter: 0, likes_counter: 0)
+      get '/users'
+    end
+
+    before do
+      subject
+    end
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it 'include placeholder LIST OF USERS' do
-      expect(response.body).to include('This a List of the Users')
+    it 'include correct number of posts' do
+      expect(response.body).to include('Number of posts: 3')
     end
 
-    it 'should render the index template' do
+    it 'should render index' do
       expect(response).to render_template(:index)
     end
   end
 
   describe 'GET /show' do
-    subject { get '/users/:id' }
+    subject do
+      first_user = User.create(name: 'Tom', photo: '', bio: 'Teacher from Mexico.', posts_counter: 0)
+      second_user = User.create(name: 'Lilly', photo: '', bio: 'Teacher from Poland.', posts_counter: 0)
+      first_post = Post.create(author: first_user, title: 'Title 1', text: 'post text 1', comments_counter: 0,
+                               likes_counter: 0)
+
+      Post.create(author: first_user, title: 'Title 2', text: 'post text 2', comments_counter: 0, likes_counter: 0)
+      Post.create(author: first_user, title: 'Title 3', text: 'post text 3', comments_counter: 0, likes_counter: 0)
+      Post.create(author: first_user, title: 'Title 4', text: 'post text 4', comments_counter: 0, likes_counter: 0)
+      Comment.create(post: first_post, author: second_user, text: 'Comment 1')
+      Comment.create(post: first_post, author: second_user, text: 'Comment 2')
+      Comment.create(post: first_post, author: second_user, text: 'Comment 3')
+      Comment.create(post: first_post, author: second_user, text: 'Comment 4')
+      Comment.create(post: first_post, author: second_user, text: 'Comment 5')
+      Comment.create(post: first_post, author: second_user, text: 'Comment 6')
+      get "/users/#{first_user.id}"
+    end
+
     before { subject }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it 'include placeholder of User and his information and posts list' do
-      expect(response.body).to include('Every User data and all his posts')
+    it 'include See All Posts button' do
+      expect(response.body).to include('See All Posts')
     end
 
-    it 'should render the show template' do
+    it 'should render show' do
       expect(response).to render_template(:show)
     end
   end
